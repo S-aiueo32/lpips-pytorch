9aba7332ecab9c9af34b9787e5b40962f962f995
Jacob Logas <logasja@gatech.edu>
2023-09-29 12:35:31 -0400

    Update setup.py
    Deal with issue that setuptools has with "latest" as a version name.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lpips_pytorch',
-    version='latest',
+    version='0.0.1',
     description='LPIPS as a Package.',
     packages=['lpips_pytorch', 'lpips_pytorch.modules'],
     author='So Uchida',
